19b34f85653724fe7cf557f2f740c02dbdf24cf0
prerantej <prerantejkvr@gmail.com>
2024-04-29 19:01:15 +0530

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { RiFacebookCircleFill } from "react-icons/ri";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { FaLinkedin } from "react-icons/fa";
+const Footer = () => {
+    return (
+        <footer className="footer bg-gray-900 text-white py-8">
+            <div className="container mx-auto flex flex-wrap justify-between">
+                <div className="w-full lg:w-1/3 mb-8 lg:mb-0">
+                    <h4 className="text-xl font-semibold mb-4">What is CampusCollab?</h4>
+                    <p>CampusCollab is a web-based platform that allows university students to effortlessly search, download, and upload academic materials corresponding to their specific course requirements. The website will feature a comprehensive database of educational resources categorized by university, semester, course, and type of material.</p>
+                </div>
+                
+                <div className="w-full lg:w-1/4 mb-8 lg:mb-0">
+                    <h4 className="text-xl font-semibold mb-4">Quick Links</h4>
+                    <ul>
+                        <li><a href="/" className="text-gray-400 hover:text-white">Homepage</a></li>
+                        <li><a href="/user-guide" className="text-gray-400 hover:text-white">User Guide</a></li>
+                        <li><a href="/login" className="text-gray-400 hover:text-white">Login</a></li>
+                        <li><a href="/signup" className="text-gray-400 hover:text-white">Signup</a></li>
+                        <li><a href="/contact-us" className="text-gray-400 hover:text-white">Contact us</a></li>
+                    </ul>
+                </div>
+
+                {/* Social Media Icons */}
+                {/* You can replace '#' with the actual links to your social media pages */}
+                <div className="w-full lg:w-1/4 mb-8 lg:mb-0">
+                    <h4 className="text-xl font-semibold mb-4">Follow Us</h4>
+                    <div className="flex space-x-4">
+                        {/* Facebook Icon */}
+                        <a href="#" className="text-gray-400 hover:text-white">
+                        <RiFacebookCircleFill  size={28}/>
+                        </a>
+                        {/* Twitter Icon */}
+                        <a href="#" className="text-gray-400 hover:text-white">
+                        <AiFillTwitterCircle size={29}/>
+                        </a>
+                        {/* LinkedIn Icon */}
+                        <a href="#" className="text-gray-400 hover:text-white">
+                        <FaLinkedin size={28}/>
+                        </a>
+                        {/* Add your icons here */}
+                    </div>
+                </div>
+
+                {/* Copyright Information */}
+                <div className="w-full text-center lg:text-right">
+                    <p className="text-gray-400">© {new Date().getFullYear()} CampusCollab. All rights reserved.</p>
+                </div>
+            </div>
+        </footer> 
+    );
+}
+
+export default Footer;
