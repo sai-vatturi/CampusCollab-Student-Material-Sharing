947805a2bcde51c2df1491eebf540e8088886230
Sainadh V <sainadhvatturi@gmail.com>
2024-05-08 22:25:59 +0530

    Modified User Guide page

--- src/UserGuide.jsx
+++ src/UserGuide.jsx
@@ -5,6 +5,11 @@ const UserGuide = () => {
     return (
         <div>
             <Navbar />
+            <div className="bg-[#FAFAFA] w-full h-screen">
+                <br />
+                <br />
+                <div className="w-[85%] bg-[#ECECFE] h-[400px] rounded-3xl mx-auto mt-5"></div>
+            </div>
         </div>
     );
 };
